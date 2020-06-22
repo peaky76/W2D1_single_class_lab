88c77b3bbca660601bce093a046f6d6dba355e52
peaky76 <robertjamespeacock@gmail.com>
2020-06-22 15:18:36 +0100

    create test_check_player

--- specs/single_class_lab_team_spec.rb
+++ specs/single_class_lab_team_spec.rb
@@ -1,12 +1,3 @@
-# ### Part B
-
-# Now we would like you to make a class that represents a sports team.
-
-# * Make a class to represent a Team that has the properties Team name (String), Players (array of strings) and a Coach (String).
-# * For each property in the class make a getter method than can return them.
-# * Create a setter method to allow the coach's name to be updated.
-# * Refactor the class to use `attr_reader` or `attr_accessor` instead of your own getter and setter methods.
-# * Create a method that adds a new player to the players array.
 # * Add a method that takes in a string of a player's name and checks to see if they are in the players array.
 # * Add a points property into your class that starts at 0.
 # * Create a method that takes in whether the team has won or lost and updates the points property for a win.
@@ -50,5 +41,16 @@ class TestTeam < Minitest::Test
         updated_players = ["Smith", "Jones", "Atkinson"]
         assert_equal(updated_players, @team1.players())
     end
+
+    def test_check_player_true()
+        @team2.check_player("Davro")
+        assert(true)
+    end
+
+    def test_check_player_false()
+        @team2.check_player("Monkhouse")
+        assert(true)
+    end
+
 
 end
